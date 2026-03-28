7dd188226556269133aacb9fd84d4a1909833ce7
PizzaLvr49 <jhtodd2012@gmail.com>
2026-03-28 14:58:51 -0400

    Refactor code structure for improved readability and maintainability

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,67 @@
-#![forbid(unsafe_code)]
-#![deny(clippy::all)]
-#![deny(clippy::pedantic)]
-#![allow(clippy::float_cmp)]
-
 mod math;
 
-use bevy::prelude::*;
-use bevy_egui::{EguiContexts, EguiPlugin, EguiPrimaryContextPass, egui};
+use std::sync::Arc;
+
+use bevy::{
+    prelude::*,
+    window::{PresentMode, WindowMode},
+};
+use bevy_egui::{
+    EguiContexts, EguiPlugin, EguiStartupSet,
+    egui::{FontData, FontDefinitions, FontFamily},
+};
 
 fn main() -> AppExit {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                mode: WindowMode::BorderlessFullscreen(MonitorSelection::Current),
+                present_mode: PresentMode::Mailbox,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugins(EguiPlugin::default())
-        .add_systems(Startup, setup_camera_system)
-        .add_systems(EguiPrimaryContextPass, ui_example_system)
-        .insert_resource(UiState {
-            is_window_open: true,
-            apoapsis: 120.0,
-            periapsis: 80.0,
-            eccentricity: 0.17,
-        })
+        .add_systems(
+            PreStartup,
+            setup_system.before(EguiStartupSet::InitContexts),
+        )
+        .add_systems(Startup, setup_fonts)
         .run()
 }
 
-#[derive(Resource)]
-struct UiState {
-    is_window_open: bool,
-    apoapsis: f64,
-    periapsis: f64,
-    eccentricity: f64,
-}
-
-fn setup_camera_system(mut commands: Commands) {
+fn setup_system(mut commands: Commands) {
     commands.spawn(Camera2d);
 }
 
-fn ui_example_system(mut contexts: EguiContexts, mut ui_state: ResMut<UiState>) -> Result {
-    let UiState {
-        is_window_open,
-        apoapsis,
-        periapsis,
-        eccentricity,
-    } = &mut *ui_state;
-
-    egui::SidePanel::left("toolbar")
-        .resizable(false)
-        .default_width(140.0)
-        .show(contexts.ctx_mut()?, |ui| {
-            ui.heading("Widgets");
-            ui.separator();
-
-            ui.toggle_value(is_window_open, "Orbit");
-        });
-
+fn setup_fonts(mut contexts: EguiContexts) -> Result {
     let ctx = contexts.ctx_mut()?;
 
-    egui::Window::new("Orbit Widget")
-        .constrain_to(ctx.available_rect())
-        .vscroll(true)
-        .open(is_window_open)
-        .show(ctx, |ui| {
-            ui.heading("Orbit");
+    let mut fonts = FontDefinitions::default();
 
-            ui.separator();
+    fonts.font_data.insert(
+        "droid_sans".to_owned(),
+        Arc::new(FontData::from_static(include_bytes!(
+            "../fonts/droid-sans.regular.ttf"
+        ))),
+    );
 
-            ui.horizontal(|ui| {
-                ui.label("Apoapsis:");
-                ui.add(
-                    egui::DragValue::new(apoapsis)
-                        .range(0.0..=f64::INFINITY)
-                        .suffix(" km"),
-                );
-            });
+    fonts.font_data.insert(
+        "droid_sans_mono".to_owned(),
+        Arc::new(FontData::from_static(include_bytes!(
+            "../fonts/droid-sans-mono.regular.ttf"
+        ))),
+    );
 
-            ui.horizontal(|ui| {
-                ui.label("Periapsis:");
-                ui.add(
-                    egui::DragValue::new(periapsis)
-                        .range(0.0..=f64::INFINITY)
-                        .suffix(" km"),
-                );
-            });
+    fonts
+        .families
+        .insert(FontFamily::Proportional, vec!["droid_sans".to_owned()]);
 
-            ui.horizontal(|ui| {
-                ui.label("Eccentricity:");
-                ui.add(
-                    egui::DragValue::new(eccentricity)
-                        .speed(0.01)
-                        .range(0.0..=10.0),
-                );
-            });
+    fonts
+        .families
+        .insert(FontFamily::Monospace, vec!["droid_sans_mono".to_owned()]);
 
-            ui.separator();
-
-            ui.horizontal(|ui| {
-                ui.label("Calculations");
-                if ui.button("Calculate Something").clicked() {
-                    info!("Calculating Something");
-                }
-            });
-        });
+    ctx.set_fonts(fonts);
 
     Ok(())
 }
