37287d15b84fdb04d814b9b77be017dba759c9e2
PizzaLvr49 <jhtodd2012@gmail.com>
2026-03-29 19:31:13 -0400

    refactor: remove unused dependencies and streamline main application structure

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,45 @@
+mod log;
 mod math;
 
-use std::sync::Arc;
-
 use bevy::{
+    log::LogPlugin,
     prelude::*,
     window::{PresentMode, WindowMode},
 };
-use bevy_egui::{
-    EguiContexts, EguiPlugin, EguiStartupSet,
-    egui::{FontData, FontDefinitions, FontFamily},
-};
+use bevy_steamworks::SteamworksPlugin;
+use tracing::Level;
 
 fn main() -> AppExit {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                mode: WindowMode::BorderlessFullscreen(MonitorSelection::Current),
-                present_mode: PresentMode::Mailbox,
+    let mut app = App::new();
+    app.add_plugins(LogPlugin {
+        fmt_layer: log::fmt_layer,
+        custom_layer: log::custom_layer,
+        level: Level::DEBUG,
+        ..default()
+    });
+    match SteamworksPlugin::init() {
+        Ok(plugin) => {
+            app.add_plugins(plugin);
+            info!("Steamworks succesfully initialized")
+        }
+        Err(err) => warn!("Steamworks failed to initialize: {err}"),
+    }
+    app.add_plugins(
+        DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: None,
                 ..default()
-            }),
-            ..default()
-        }))
-        .add_plugins(EguiPlugin::default())
-        .add_systems(
-            PreStartup,
-            setup_system.before(EguiStartupSet::InitContexts),
-        )
-        .add_systems(Startup, setup_fonts)
-        .run()
+            })
+            .disable::<LogPlugin>(),
+    )
+    .add_systems(Startup, spawn_window)
+    .run()
 }
 
-fn setup_system(mut commands: Commands) {
-    commands.spawn(Camera2d);
-}
-
-fn setup_fonts(mut contexts: EguiContexts) -> Result {
-    let ctx = contexts.ctx_mut()?;
-
-    let mut fonts = FontDefinitions::default();
-
-    fonts.font_data.insert(
-        "droid_sans".to_owned(),
-        Arc::new(FontData::from_static(include_bytes!(
-            "../fonts/droid-sans.regular.ttf"
-        ))),
-    );
-
-    fonts.font_data.insert(
-        "droid_sans_mono".to_owned(),
-        Arc::new(FontData::from_static(include_bytes!(
-            "../fonts/droid-sans-mono.regular.ttf"
-        ))),
-    );
-
-    fonts
-        .families
-        .insert(FontFamily::Proportional, vec!["droid_sans".to_owned()]);
-
-    fonts
-        .families
-        .insert(FontFamily::Monospace, vec!["droid_sans_mono".to_owned()]);
-
-    ctx.set_fonts(fonts);
-
-    Ok(())
+fn spawn_window(mut commands: Commands) {
+    commands.spawn(Window {
+        mode: WindowMode::BorderlessFullscreen(MonitorSelection::Current),
+        present_mode: PresentMode::Mailbox,
+        ..default()
+    });
 }
